fc444e1098935ab5dae8c143979a1bbb726183ad
jinkang-0 <jinkang.fang@gmail.com>
2025-10-06 18:36:34 -0700

    [feat] add vitest for unit tests

---
+++ tests/browser.test.ts
@@ -0,0 +1,13 @@
+// @vitest-environment jsdom
+
+import { expect, test, vi } from 'vitest';
+import { loadEnvironment } from '@/lib/test-utils';
+
+vi.stubEnv('NODE_ENV', 'production');
+loadEnvironment();
+
+// run tests
+test('environment variables are set', () => {
+  expect(process.env.NEXT_PUBLIC_SUPABASE_URL).toBeDefined();
+  expect(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY).toBeDefined();
+});

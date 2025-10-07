5d78560ec7140bcec5b1bdf2f6d060979e2d42fd
jinkang-0 <jinkang.fang@gmail.com>
2025-10-06 19:04:21 -0700

    [chore] add note explaining caveats

--- tests/browser.test.ts
+++ tests/browser.test.ts
@@ -14,4 +14,17 @@ test('environment is browser', () => {
 test('environment variables are set', () => {
   expect(process.env.NEXT_PUBLIC_SUPABASE_URL).toBeDefined();
   expect(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY).toBeDefined();
+
+  /**
+   * Other environment variables are still technically
+   * visible. This is because Next.js actually does not
+   * export the env file, but rather replaces every use
+   * of NEXT_PUBLIC_* environment variables with the value
+   * itself. This test simply loads the env file, which operates
+   * differently.
+   *
+   * Nonetheless, we shouldn't have to worry about exposed
+   * environment variables to the client as long as the
+   * secrets are not prefixed by NEXT_PUBLIC.
+   */
 });

621557bf1cd495bd2423857982f5269ac91d8c37
jinkang-0 <jinkang.fang@gmail.com>
2025-10-10 15:46:14 -0700

    [feat] add sign out button in profile

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -1,10 +1,24 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { signOut } from '@/actions/auth';
 import CustomLink from '@/components/CustomLink';
 
 export default function ProfilePage() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    const { error } = await signOut();
+
+    if (error) alert(error.message);
+    else router.push('/');
+  };
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <CustomLink href="/">← Go back</CustomLink>
       <p>Profile page</p>
+      <button onClick={() => handleSignOut()}>Sign out</button>
     </div>
   );
 }

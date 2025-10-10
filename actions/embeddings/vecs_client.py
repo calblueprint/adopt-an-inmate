056a3baa83e915338c3953d431ea02cc8c5ca69d
carolynzhuang <cwz@berkeley.edu>
2025-10-10 03:37:45 -0700

    fix some other stuff

--- actions/embeddings/vecs_client.py
+++
@@ -1,8 +0,0 @@
-import supabase
-from config import SUPABASE_URL, SUPABASE_ANON_KEY
-
-supabase_client = supabase.create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
-
-def get_adoptees():
-    response = supabase_client.table("adoptee").select("*").execute()
-    return response.data

611298528c56ec7ea89237182aff5d3ff76d33e9
carolynzhuang <cwz@berkeley.edu>
2025-10-10 02:26:54 -0700

    finish python script to create embeddings

---
+++ actions/embeddings/embed.py
@@ -0,0 +1,35 @@
+from config import supabase_client, adoptee_vector, vx, model
+
+def store_data():
+  """Store the vector information in the adoptee_vector table."""
+
+  adoptee = supabase_client.table("adoptee").select("*").execute().data
+
+  records = []
+
+  for row in adoptee:
+    row_id = row['id']
+    text = row['bio']
+    embedding = model.encode(text).tolist()
+
+    metadata = {
+      "bio": row["bio"], 
+      "gender": row["gender"],
+      "age": row["age"],
+      "veteran_status": row["veteran_status"],
+      "offense": row["offense"],
+      "state": row["state"]
+    }
+
+    records.append(((row_id, embedding, metadata)))
+
+  try:
+    adoptee_vector.upsert(records)
+  except Exception as e:
+    print("Upsert failed:", e)
+
+  vx.disconnect()
+
+if __name__ == "__main__":
+  store_data()
+  
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,7 @@ const eslintConfig = [
       'build',
       '.vscode',
       'next-env.d.ts',
+      'adopt-an-inmate-venv',
     ],
   },
   {

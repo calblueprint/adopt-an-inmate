0c8c5b1bd506cb2c195194a74bb8ed5822cd68ef
plaanty <145934497+plaanty@users.noreply.github.com>
2025-10-12 16:04:31 -0700

    Added the photo to the login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image';
-import Logo from '@/assets/images/Adopt_An_Inmate_logo.png';
+import Logo from '@/assets/images/Adopt_An_Inmate_logo_even_better.png';
 import { LogInCard } from '@/components/LoginCard';
 
 export default function LoginPage() {
   return (
     <div className="h-full w-full bg-[#EDEBE9]">
       <div className="flex flex-col">
-        <Image src={Logo} width={100} height={100} alt="Logo" />
+        <Image
+          src={Logo}
+          width={53.981}
+          height={42.055}
+          alt="Logo"
+          className="mt-[38px]"
+        />
         <div className="flex h-full w-full flex-col items-center justify-items-center">
           <LogInCard />
         </div>

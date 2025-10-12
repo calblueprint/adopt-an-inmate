e27be1671f63d45f039b9c6269bbebd8c5962175
plaanty <145934497+plaanty@users.noreply.github.com>
2025-10-12 16:17:15 -0700

    Added the image to the signup page as well

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import Image from 'next/image';
-import Logo from '@/assets/images/Adopt_An_Inmate_logo.png';
+import Logo from '@/assets/images/Adopt_An_Inmate_logo_even_better.png';
 import { SignUpCard } from '@/components/SignUpCard';
 
 export default function SignUpPage() {
   return (
     // This is the background
     <div className="h-full w-full bg-[#EDEBE9]">
-      <div className="flex flex-col">
-        <Image src={Logo} width={100} height={100} alt="Logo" />
+      <div className="flex flex-col items-center gap-[97px]">
+        <Image src={Logo} width={53.981} height={42.055} alt="Logo" />
         <div className="flex h-full w-full flex-col items-center justify-items-center">
           <SignUpCard />
         </div>

2d450317504bebfb063a5ae3f3c38b105333ed9d
jinkang-0 <jinkang.fang@gmail.com>
2025-10-13 01:54:29 -0700

    [fix] remove redirect in middleware

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -42,21 +42,7 @@ export async function updateSession(request: NextRequest) {
   // IMPORTANT: DO NOT REMOVE auth.getUser()
 
   // Calling auth.getUser() refreshes the auth token (updates cookies)
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/error')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
-  }
+  await supabase.auth.getUser();
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:

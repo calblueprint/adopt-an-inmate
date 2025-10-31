eda7b83f1cb3cab4b77c7a76fd67d4df0307b6ae
plaanty <145934497+plaanty@users.noreply.github.com>
2025-10-30 22:02:35 -0700

    Made a lot of progress with the sprint

---
+++ components/ProfileButton.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function ProfileButton() {
+  return (
+    <button
+      className={`rounded-2xl px-19 py-22 font-medium transition-all duration-200`}
+    >
+      Carolyn Zhuang
+    </button>
+  );
+}

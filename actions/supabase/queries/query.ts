7a8ac88c4763250e8c3bf215941c1c07773fbad0
Inaya Yusuf <inayayusuf.29@gmail.com>
2025-11-03 23:56:22 -0800

    added fetch queries for apps

--- actions/supabase/queries/query.ts
+++ actions/supabase/queries/query.ts
@@ -24,12 +24,28 @@ export async function fetchTopK(embedding: number[], k_value: number) {
   return data;
 }
 
-export async function fetchApplication() {
-  //app_UUID: string
-  const { data, error } = await supabase.from('your_table_name').select('*');
+export async function fetchApplication(app_UUID: string) {
+  const { data, error } = await supabase
+    .from('adopter_applications_dummy')
+    .select('*')
+    .eq('app_uuid', app_UUID)
+    .maybeSingle();
 
   if (error) {
-    throw new Error(`Error fetching data: ${error.message}`);
+    throw new Error(`Error fetching application: ${error.message}`);
+  }
+
+  return data;
+}
+
+export async function fetchUserApplicationUUIDs(adopter_UUID: string) {
+  const { data, error } = await supabase
+    .from('adopter_applications_dummy')
+    .select('*')
+    .eq('adopter_uuid', adopter_UUID);
+
+  if (error) {
+    throw new Error(`Error fetching adopter's applications: ${error.message}`);
   }
 
   return data;

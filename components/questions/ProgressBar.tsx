01f0480e5fe001aec9c20b62e6e8ccd8646e5bcb
jinkang-0 <jinkang.fang@gmail.com>
2025-11-09 21:02:04 -0800

    [chore] bar shows current progress instead of max

--- components/questions/ProgressBar.tsx
+++ components/questions/ProgressBar.tsx
@@ -11,7 +11,7 @@ export default function ProgressBar({
   stepsToSkip?: number;
 }) {
   const currentQuestion = useCurrentQuestion();
-  const { questionsCompleted, questions } = useQuestionsContext();
+  const { questions } = useQuestionsContext();
   const numQuestions = useMemo(
     () => Math.max(0, questions.length - stepsToSkip),
     [questions, stepsToSkip],
@@ -22,7 +22,7 @@ export default function ProgressBar({
       className={cn('flex gap-1', currentQuestion >= numQuestions && 'hidden')}
     >
       {[...Array(numQuestions).keys()].map((k, idx) => (
-        <ProgressBarNode key={k} highlighted={idx < questionsCompleted} />
+        <ProgressBarNode key={k} highlighted={idx < currentQuestion} />
       ))}
     </div>
   );

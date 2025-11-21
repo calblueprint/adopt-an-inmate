0bfe541566834fbd3ee8513f2e6a1e41e36017f2
carolynzhuang <cwz@berkeley.edu>
2025-11-21 15:43:32 -0800

    create ranking feat

--- components/application/matching/MatchingCard.tsx
+++ components/application/matching/MatchingCard.tsx
@@ -10,14 +10,20 @@ import { AdopteeMatch } from '@/types/types';
 export default function MatchingCard({
   match,
   matchIndex,
+  rank,
+  onSelect,
 }: {
   match: AdopteeMatch;
   matchIndex: number;
+  rank?: number;
+  onSelect: (id: string) => void;
 }) {
   const bioElmt = useRef<HTMLParagraphElement>(null);
   const [isOverflowing, setIsOverflowing] = useState(false);
   const searchParams = useSearchParams();
   const router = useRouter();
+
+  const isSelected = !(rank === undefined);
 
   useEffect(() => {
     if (typeof window === 'undefined') return;
@@ -41,28 +47,46 @@ export default function MatchingCard({
     };
   }, []);
 
-  const onShowMore = () => {
+  const onShowMore = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     const params = new URLSearchParams(searchParams);
     params.set('details', matchIndex.toString());
     router.push(`?${params.toString()}`);
   };
 
+  const handleCardClick = () => {
+    onSelect(match.id);
+  };
+
   return (
-    <div className="flex flex-1 cursor-pointer flex-col gap-6 rounded-lg border border-gray-6 bg-gray-1 p-8 shadow-md transition-colors hover:border-gray-12 has-[button:hover]:border-gray-6!">
+    <div
+      onClick={handleCardClick}
+      className={`relative flex flex-1 cursor-pointer flex-col gap-6 rounded-lg border p-8 shadow-md transition-all ${
+        isSelected
+          ? 'border-4 border-red-12'
+          : 'border-4 border-transparent hover:border-red-12'
+      } bg-gray-1 has-[button:hover]:border-gray-6!`}
+    >
+      {/* rank badge */}
+      {rank !== undefined && (
+        <div className="absolute top-0 left-0 flex h-12 w-12 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-red-12">
+          <h4 className="font-bold text-white">{rank}</h4>
+        </div>
+      )}
       {/* name and metadata */}
       <div className="flex flex-col gap-1">
         <h1>{match.first_name}</h1>
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-1">
-            <LuCalendar size={16} />
+            <LuCalendar size={16} className="text-red-12" />
             <p>{calculateAge(match.dob)}</p>
           </div>
           <div className="flex items-center gap-1">
-            <LuUser size={16} />
+            <LuUser size={16} className="text-red-12" />
             <p className="capitalize">{match.gender}</p>
           </div>
           <div className="flex items-center gap-1">
-            <LuMapPin size={16} />
+            <LuMapPin size={16} className="text-red-12" />
             <p>{getStateAbbv(match.state)}</p>
           </div>
         </div>

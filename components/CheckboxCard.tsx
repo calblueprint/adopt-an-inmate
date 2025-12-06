7f28803c6fc4586e5a7a9ee23d20f4cc3023ebd8
Sai Divyesh Tunguturu <67570329+saitunguturu@users.noreply.github.com>
2025-12-06 02:05:00 -0800

    added Offense Question to main application (#88)
    * added Offense Question to main application
    
    * changed appearance of multi-select options to look like a square checklist

---
+++ components/CheckboxCard.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { useId } from 'react';
+import { cn } from '@/lib/utils';
+
+export default function CheckboxCard({
+  children,
+  className,
+  ...props
+}: Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type' | 'id'>) {
+  const id = useId();
+
+  return (
+    <label
+      className="flex cursor-pointer items-center gap-3 rounded-lg border border-gray-7 px-2 py-1 transition-colors has-[input:checked]:border-red-12"
+      htmlFor={id}
+    >
+      <input
+        type="checkbox"
+        id={id}
+        className={cn(
+          className,
+          'relative size-4 appearance-none rounded border-2 border-red-12 transition-colors after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:text-red-12 after:opacity-0 after:transition-opacity after:content-["✓"] checked:after:opacity-100',
+        )}
+        {...props}
+      />
+      {children}
+    </label>
+  );
+}

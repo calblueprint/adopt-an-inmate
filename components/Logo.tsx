a9bdcc837dbc57e1e92627e51397fe5880696c43
Ethan Tam <ethantam@berkeley.edu>
2025-12-10 00:49:26 -0800

    change padding

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import LogoImg from '@/assets/images/Adopt_An_Inmate_logo.png';
 
 export default function Logo() {
   return (
-    <div className="flex flex-row items-center gap-6 pt-14 pb-4">
+    <div className="flex flex-row items-center gap-6 pt-14 pb-3">
       <Image src={LogoImg} height={42} alt="Logo" priority />
       <p className="font-golos text-xl font-extrabold text-red-9">
         {' '}

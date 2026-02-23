090ad629b0b3a3c10be3e22bafd90bd1827c6ddb
jinkang-0 <jinkang.fang@gmail.com>
2026-02-23 00:45:57 -0800

    [fix] comment out test route functionality to patch security vulnerability mid-development

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,33 +1,35 @@
-import createAdopterApplication from '@/actions/monday/mutations/createAdopterApplication';
+// import createAdopterApplication from '@/actions/monday/mutations/createAdopterApplication';
 
 export async function GET() {
-  const { success, error } = await createAdopterApplication(
-    {
-      date_of_birth: new Date('2000-01-01').toISOString().split('T')[0],
-      first_name: 'Blueprint',
-      last_name: 'Test',
-      pronouns: 'he/him',
-      state: 'California',
-      user_id: 'asd',
-      veteran_status: false,
-      monday_id: null,
-    },
-    {
-      adopter_uuid: 'asd',
-      app_uuid: 'asd',
-      gender_pref: 'No preference',
-      offense_pref: [],
-      personal_bio: 'Blueprint Test',
-      ranked_cards: [],
-      return_explanation: '',
-      status: 'pending',
-      time_submitted: new Date().toISOString().split('T')[0],
-      monday_id: null,
-      exported_to_monday: false,
-      age_pref: [],
-    },
-  );
+  // const { success, error } = await createAdopterApplication(
+  //   {
+  //     date_of_birth: new Date('2000-01-01').toISOString().split('T')[0],
+  //     first_name: 'Blueprint',
+  //     last_name: 'Test',
+  //     pronouns: 'he/him',
+  //     state: 'California',
+  //     user_id: 'asd',
+  //     veteran_status: false,
+  //     monday_id: null,
+  //   },
+  //   {
+  //     adopter_uuid: 'asd',
+  //     app_uuid: 'asd',
+  //     gender_pref: 'No preference',
+  //     offense_pref: [],
+  //     personal_bio: 'Blueprint Test',
+  //     ranked_cards: [],
+  //     return_explanation: '',
+  //     status: 'pending',
+  //     time_submitted: new Date().toISOString().split('T')[0],
+  //     monday_id: null,
+  //     exported_to_monday: false,
+  //     age_pref: [],
+  //   },
+  // );
 
-  if (success) return new Response('Row inserted successfully.');
-  return new Response(`${new String(error)}`);
+  // if (success) return new Response('Row inserted successfully.');
+  // return new Response(`${new String(error)}`);
+
+  return new Response('Test');
 }

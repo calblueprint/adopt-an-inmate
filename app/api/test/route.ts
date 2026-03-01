92601a1fc601c06083c2559434a407e0b5f52aac
jinkang-0 <jinkang.fang@gmail.com>
2026-02-28 22:06:58 -0800

    [refactor] check data integrity before export to monday

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,35 +1,10 @@
-// import createAdopterApplication from '@/actions/monday/mutations/createAdopterApplication';
+import exportApplication from '@/actions/monday/mutations/exportApplication';
 
 export async function GET() {
-  // const { success, error } = await createAdopterApplication(
-  //   {
-  //     date_of_birth: new Date('2000-01-01').toISOString().split('T')[0],
-  //     first_name: 'Blueprint',
-  //     last_name: 'Test',
-  //     pronouns: 'he/him',
-  //     state: 'California',
-  //     user_id: 'asd',
-  //     veteran_status: false,
-  //     monday_id: null,
-  //   },
-  //   {
-  //     adopter_uuid: 'asd',
-  //     app_uuid: 'asd',
-  //     gender_pref: 'No preference',
-  //     offense_pref: [],
-  //     personal_bio: 'Blueprint Test',
-  //     ranked_cards: [],
-  //     return_explanation: '',
-  //     status: 'pending',
-  //     time_submitted: new Date().toISOString().split('T')[0],
-  //     monday_id: null,
-  //     exported_to_monday: false,
-  //     age_pref: [],
-  //   },
-  // );
+  const { success, error } = await exportApplication(
+    '0c2650b0-f188-4253-8182-735f9eeb35cf',
+  );
 
-  // if (success) return new Response('Row inserted successfully.');
-  // return new Response(`${new String(error)}`);
-
-  return new Response('Test');
+  if (success) return new Response('Row inserted successfully.');
+  return new Response(`${new String(error)}`);
 }

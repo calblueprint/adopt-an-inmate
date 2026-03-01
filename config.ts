374e131d216b5622c24fd437b1a775f7f7c4b027
jinkang-0 <jinkang.fang@gmail.com>
2026-02-28 21:27:34 -0800

    [chore] add env var asserts + config for mutation

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  // If true, it will allow the server to issue Monday mutation
+  // calls. ONLY ENABLE IF THE CLIENT->SERVER API CALL IS SAFE FROM ATTACKS.
+  enableMondayMutations: false,
+};

ff260f0de772f956da0cf0c58fa17ef9dc015f93
jinkang-0 <jinkang.fang@gmail.com>
2026-03-05 01:18:59 -0800

    [chore] update example.env

--- actions/monday/core.ts
+++ actions/monday/core.ts
@@ -1,8 +1,8 @@
 import { ApiClient } from '@mondaydotcomorg/api';
 import { assertEnvVarExists } from '@/lib/utils';
 
-assertEnvVarExists('MONDAY_API_TOKEN');
+assertEnvVarExists('MONDAY_API_KEY');
 
 export const mondayApiClient = new ApiClient({
-  token: process.env.MONDAY_API_TOKEN ?? '',
+  token: process.env.MONDAY_API_KEY ?? '',
 });

fa7dc4fd16c6df4b08959086afe4f26c98f6b2d7
jinkang-0 <jinkang.fang@gmail.com>
2026-03-08 00:26:25 -0800

    [feat] setup jwt checking in webhook handler

--- app/api/monday-webhook/route.ts
+++ app/api/monday-webhook/route.ts
@@ -1,12 +1,28 @@
 import { NextRequest, NextResponse } from 'next/server';
+import jwt from 'jsonwebtoken';
+import Logger from '@/actions/logging';
+import { assertEnvVarExists, getEnvVar } from '@/lib/utils';
+
+assertEnvVarExists('MONDAY_SIGNING_SECRET');
 
 export async function POST(request: NextRequest) {
-  // verify jwt from request
+  const signingSecret = getEnvVar('MONDAY_SIGNING_SECRET');
+
+  const authHeader = request.headers.get('authorization');
+  if (!authHeader) return new NextResponse(request.body);
+
+  // verify jwt from request auth header
+  try {
+    jwt.verify(authHeader, signingSecret);
+  } catch (error) {
+    Logger.error(`Error decoding JWT from Monday webhook: ${error}`);
+    return new NextResponse(request.body);
+  }
 
   // parse events
+  const data = await request.json();
+  console.log(data);
 
   // respond to challenge
-  console.log(JSON.stringify(request.body));
-
-  return new NextResponse(request.body);
+  return Response.json({ data });
 }

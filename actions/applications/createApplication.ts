647c0639186c63814e235d326a0d3088e9cbec3c
Jinkang Fang <67077248+jinkang-0@users.noreply.github.com>
2026-03-28 15:31:28 -0700

    [feat] implement create application constraints (#158)
    * [feat] add create application constraints
    
    * [patch] add logic for missing checks
    
    * [patch] delete extracted function for 'continue application'
    
    * [bug] account for only past active apps case
    
    ---------
    
    Co-authored-by: Inaya Yusuf <inayayusuf.29@gmail.com>

---
+++ actions/applications/createApplication.ts
@@ -0,0 +1,164 @@
+'use server';
+
+import { User } from '@supabase/supabase-js';
+import { getSupabaseServerClient } from '@/lib/supabase';
+import { AdopterApplication } from '@/types/schema';
+import Logger from '../logging';
+
+/**
+ * Checks the requesting user's ability to create
+ * a new application. Returns true if the user is
+ * able to create, false otherwise.
+ */
+export const checkCreationConstraints = async (user: User) => {
+  const supabase = await getSupabaseServerClient();
+
+  const { data: profile, error: getProfileError } = await supabase
+    .from('adopter_profiles')
+    .select()
+    .eq('user_id', user.id)
+    .maybeSingle();
+
+  if (getProfileError) {
+    Logger.error(
+      `Error getting adopter profile for ${user.id}: ${getProfileError}`,
+    );
+    return { data: false, error: 'An unexpected error occurred.' };
+  }
+
+  if (!profile) {
+    return { data: false, error: 'User has no profile.' };
+  }
+
+  // constraint: invalid ended reason
+  if (profile.past_inactive_reason === 'NPO_CANCELLED') {
+    return { data: false, error: 'User has an invalid ended reason.' };
+  }
+
+  // fetch applications
+  const { data: appsData, error: getAppError } = await supabase
+    .from('adopter_applications_dummy')
+    .select()
+    .eq('adopter_uuid', user.id);
+
+  if (getAppError) {
+    Logger.error(`Error getting app data for ${user.id}: ${getAppError}`);
+    return { data: false, error: 'An unexpected error occurred.' };
+  }
+
+  // constraint: check number of active adoptees
+  const numActiveApps = appsData.filter(
+    app => app.status === 'ACCEPTED',
+  ).length;
+  const totalActiveAdoptees = numActiveApps + (profile.num_past_active || 0);
+
+  if (totalActiveAdoptees >= 2) {
+    return {
+      data: false,
+      error: 'User already has at least two past active adoptees',
+    };
+  }
+
+  if (!appsData || appsData.length === 0) {
+    return { data: true, error: null };
+  }
+
+  const now = new Date();
+
+  for (const app of appsData) {
+    // constraint: was rejected
+    if (app.status === 'REJECTED') {
+      return { data: false, error: 'Application was rejected, contact NPO.' };
+    }
+
+    // constraint: has existing app
+    const existingStatus: AdopterApplication['status'][] = [
+      'INCOMPLETE',
+      'PENDING',
+    ];
+
+    if (existingStatus.includes(app.status)) {
+      return { data: false, error: 'An application is already in progress.' };
+    }
+
+    if (!app.time_submitted) continue;
+
+    // constraint: 6mo recent
+    const submittedTime = new Date(app.time_submitted);
+    const yearDelta = now.getFullYear() - submittedTime.getFullYear();
+    const yearMod = yearDelta === 0 ? 0 : 12 - submittedTime.getMonth();
+    const yearRem = yearDelta > 1 ? (yearDelta - 1) * 12 : 0;
+    const monthDelta =
+      now.getMonth() - submittedTime.getMonth() + yearMod + yearRem;
+    const dayDelta = now.getDate() - submittedTime.getDate();
+
+    if (monthDelta < 6 || (monthDelta === 6 && dayDelta < 0)) {
+      return {
+        data: false,
+        error: 'An application was already submitted within 6 months.',
+      };
+    }
+  }
+
+  return { data: true, error: null };
+};
+
+/**
+ * Creates a new application if the requester is
+ * able to. Returns the appId on success, an error
+ * otherwise.
+ */
+export const createApplication = async () => {
+  const supabase = await getSupabaseServerClient();
+
+  // get user auth
+  const {
+    data: { user },
+    error: getUserError,
+  } = await supabase.auth.getUser();
+
+  if (getUserError) {
+    Logger.error(`Error fetching user: ${getUserError}`);
+    return { data: null, error: 'An unexpected error occurred.' };
+  }
+
+  if (!user) {
+    return { data: null, error: 'User not logged in' };
+  }
+
+  // check create app constraint
+  const { data: canCreate, error: constraintError } =
+    await checkCreationConstraints(user);
+
+  if (!canCreate || constraintError) {
+    return {
+      data: null,
+      error: constraintError || 'Cannot create application',
+    };
+  }
+
+  // create app with user id
+  const { data: insertedRow, error: insertError } = await supabase
+    .from('adopter_applications_dummy')
+    .insert({
+      adopter_uuid: user.id,
+      status: 'INCOMPLETE',
+    })
+    .select()
+    .maybeSingle();
+
+  // error handling
+  if (insertError || !insertedRow) {
+    Logger.error(
+      insertError
+        ? `Error inserting: ${insertError.message}`
+        : `No row inserted`,
+    );
+    return { data: null, error: 'An unexpected error occurred.' };
+  }
+
+  return {
+    data: insertedRow.app_uuid,
+    error: null,
+  };
+};

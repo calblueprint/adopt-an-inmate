d9b41d347e703b4cad145e9564961805b63b6051
Inaya Yusuf <inayayusuf.29@gmail.com>
2026-03-27 21:37:27 -0700

    restored lockfile, removed edit button

--- components/application/main/MainQuestionReview.tsx
+++ components/application/main/MainQuestionReview.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect } from 'react';
 import { Button } from '@/components/Button';
-import CustomLink from '@/components/CustomLink';
 import QuestionBack from '@/components/questions/QuestionBack';
 import { useApplicationContext } from '@/contexts/ApplicationContext';
 import { useAuth } from '@/contexts/AuthProvider';
@@ -36,30 +35,7 @@ export default function MainQuestionReview() {
         <header className="flex flex-col gap-2">
           <h1>Review and Submit</h1>
         </header>
-        <div className="flex items-center gap-2">
-          <CustomLink
-            href="#"
-            className="font-semibold text-cyan-11 decoration-solid underline-offset-4"
-            onClick={e => {
-              e.preventDefault();
-            }}
-          >
-            Edit
-          </CustomLink>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="21"
-            height="21"
-            viewBox="0 0 21 21"
-            fill="none"
-            className="shrink-0"
-          >
-            <path
-              d="M18.1212 6.16023C18.4625 5.81898 18.4625 5.25023 18.1212 4.92648L16.0737 2.87898C15.75 2.53773 15.1812 2.53773 14.84 2.87898L13.23 4.48023L16.5112 7.76148M2.625 15.094V18.3752H5.90625L15.5837 8.68898L12.3025 5.40773L2.625 15.094Z"
-              fill="#217574"
-            />
-          </svg>
-        </div>
+        <div className="flex items-center gap-2"></div>
       </div>
 
       {/* Scrollable Text Box*/}

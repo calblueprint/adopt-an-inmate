ba057c5f12063724e217101aa2efa63c9137e241
Inaya Yusuf <inayayusuf.29@gmail.com>
2026-04-17 01:21:05 -0700

    [chore] resolving conflicts, minor lint error

--- components/application/main/MainQuestionReview.tsx
+++ components/application/main/MainQuestionReview.tsx
@@ -7,11 +7,7 @@ import { useApplicationContext } from '@/contexts/ApplicationContext';
 import { useAuth } from '@/contexts/AuthProvider';
 import { useProfile } from '@/contexts/ProfileProvider';
 import { useApplicationNavigation } from '@/hooks/app-process';
-import { formatGenderPreference } from '@/lib/formatters';
-import {
-  formatAgePreference,
-  formatGenderPreference,
-} from '@/lib/formatters';
+import { formatAgePreference, formatGenderPreference } from '@/lib/formatters';
 import { ApplicationStage } from '@/types/enums';
 
 export default function MainQuestionReview() {

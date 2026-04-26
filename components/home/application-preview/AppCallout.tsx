aca1476b68fa30914f8d1301b0e93ddd190bf750
spect <jinkang.fang@gmail.com>
2026-04-26 11:09:47 -0700

    [patch] fix active callout link

--- components/home/application-preview/AppCallout.tsx
+++ components/home/application-preview/AppCallout.tsx
@@ -1,25 +1,34 @@
 'use client';
 
 import { useMemo } from 'react';
+import { LuArrowUpRight } from 'react-icons/lu';
+import Link from 'next/link';
 import { cva } from 'class-variance-authority';
 import { CONFIG } from '@/config';
 import { formatDate } from '@/lib/formatters';
 import { AdopterApplication } from '@/types/schema';
 
-const calloutStyles = cva('rounded-lg px-6 py-5 flex flex-col gap-1', {
-  variants: {
-    variant: {
-      PENDING: 'bg-yellow-3',
-      PENDING_CONFIRMATION: 'bg-yellow-3',
-      ACTIVE: 'bg-[#DEF4DF]',
-      ACCEPTED: 'bg-[#DEF4DF]',
-      REAPPLY: 'bg-[#EBD2FF]',
-      REJECTED: 'bg-red-5',
-      ENDED: 'bg-[#FDDEF3]',
-      INCOMPLETE: '',
+const calloutStyles = cva(
+  'rounded-lg px-6 py-5 flex flex-col gap-1 font-medium',
+  {
+    variants: {
+      status: {
+        PENDING: 'bg-yellow-3',
+        PENDING_CONFIRMATION: 'bg-yellow-3',
+        ACTIVE: 'bg-[#DEF4DF]',
+        ACCEPTED: 'bg-[#DEF4DF]',
+        REAPPLY: 'bg-[#EBD2FF]',
+        REJECTED: 'bg-red-5',
+        ENDED: 'bg-[#FDDEF3]',
+        INCOMPLETE: '',
+      },
+      variant: {
+        default: '',
+        link: 'flex-row items-center justify-between gap-2 group',
+      },
     },
   },
-});
+);
 
 export default function AppCallout({
   app,
@@ -70,14 +79,43 @@ export default function AppCallout({
     }
   }, [app]);
 
-  if (app.status === 'ACTIVE') return null;
+  if (app.status === 'ACTIVE')
+    return (
+      <CalloutLink
+        href="https://docs.google.com/document/d/1ASL6ReAo3zyODDdqjf9bP3OWAWlZQG8bplheDCYFMRM/edit?usp=sharing"
+        status={app.status}
+      >
+        Click here to see mailing regulation by state
+      </CalloutLink>
+    );
 
   return (
-    <div className={calloutStyles({ variant: app.status })}>
-      <p className="text-md font-medium">{calloutTitle}</p>
-      <p className="text-sm font-medium text-black/40">{calloutDescription}</p>
+    <div className={calloutStyles({ status: app.status })}>
+      <p className="text-md">{calloutTitle}</p>
+      <p className="text-sm text-black/40">{calloutDescription}</p>
 
       {children}
     </div>
+  );
+}
+
+function CalloutLink({
+  href,
+  children,
+  status,
+}: {
+  href: string;
+  children?: React.ReactNode;
+  status: AdopterApplication['status'];
+}) {
+  return (
+    <Link
+      href={href}
+      className={calloutStyles({ status, variant: 'link' })}
+      target="_blank"
+    >
+      {children}
+      <LuArrowUpRight className="text-gray-11 transition-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5 group-hover:scale-125" />
+    </Link>
   );
 }

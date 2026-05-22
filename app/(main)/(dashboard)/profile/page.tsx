f5444303c64fe988b35b7f37c1b3295aa76a72d9
Inaya Yusuf <inayayusuf.29@gmail.com>
2026-05-21 18:38:45 -0700

    [fix] rebase, routing, component organization

--- app/(main)/(dashboard)/profile/page.tsx
+++ app/(main)/(dashboard)/profile/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { signOut } from '@/actions/auth';
-import CustomLink from '@/components/CustomLink';
+// import { useRouter } from 'next/navigation';
+// import { signOut } from '@/actions/auth';
+// import CustomLink from '@/components/CustomLink';
 import ProfileView from '@/components/profile/ProfileView';
 
 export default function ProfilePage() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const handleSignOut = async () => {
-    const { error } = await signOut();
+  // const handleSignOut = async () => {
+  //   const { error } = await signOut();
 
-    if (error) alert(error.message);
-    else router.push('/');
-  };
+  //   if (error) alert(error.message);
+  //   else router.push('/');
+  // };
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">

dce1d76ada8ca93de5762cbdaa2a630d0cc19124
Inaya Yusuf <inayayusuf.29@gmail.com>
2026-05-21 23:47:56 -0700

    [chore] clean up comments

--- app/(main)/(dashboard)/profile/page.tsx
+++ app/(main)/(dashboard)/profile/page.tsx
@@ -1,27 +1,10 @@
 'use client';
 
-// import { useRouter } from 'next/navigation';
-// import { signOut } from '@/actions/auth';
-// import CustomLink from '@/components/CustomLink';
 import ProfileView from '@/components/profile/ProfileView';
 
 export default function ProfilePage() {
-  // const router = useRouter();
-
-  // const handleSignOut = async () => {
-  //   const { error } = await signOut();
-
-  //   if (error) alert(error.message);
-  //   else router.push('/');
-  // };
-
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
-      {/* <CustomLink href="/">← Go back</CustomLink> */}
-      {/* <p>Profile page</p> */}
-      {/* <button onClick={() => router.push('/profile/edit')}>Edit Profile</button>
-      <button onClick={() => handleSignOut()}>Sign out</button> */}
-
       <ProfileView />
     </div>
   );

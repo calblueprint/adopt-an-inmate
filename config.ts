43e3437653ebceeffb17730e65e1cb59cb62fef3
Inaya Yusuf <inayayusuf.29@gmail.com>
2026-05-22 22:58:24 -0700

    reset config

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 export const CONFIG = {
   // If true, it will allow the server to issue Monday mutation
   // calls. ONLY ENABLE IF THE CLIENT->SERVER API CALL IS SAFE FROM ATTACKS.
-  enableMondayMutations: true,
+  enableMondayMutations: false,
 
   // The point of contact email for admins
-  adminEmail: 'inayayusuf.29@gmail.com', //'adopt@adoptaninmate.org',
+  adminEmail: 'adopt@adoptaninmate.org',
 };
